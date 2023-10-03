1201913e62a80bb4567e91fcb4be02823b2c52ca
lemisora <leonardo.michel.dom@gmail.com>
2023-10-03 09:57:54 -0600

    Se añade el archivo inicial con los requisitos para crear el programa aplicado de la cola

---
+++ aplicacion_cola.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct coche{
+    char placa[10];
+    char *marca;
+    char *color;
+    char *hora;
+};
+
+struct cajon{
+    int consecutivo;
+    char id[10];
+};
+
+/*Secuencias para la entrada de un coche
+ *      --  Cola de coches
+ *  -> Alta coche
+ *      - (Se capturan los datos e incrementa el contador)
+ *
+ *  -> Generación e impresión de comprobante
+ *
+ *  -> Asigna y actualiza el cajon (opcional)
+ *
+ *
+ */
+
+/*Seuencias para la salida de un coche
+ *  -> Baja de coches
+ *
+ *
+ */
+
+int main(){
+
+
+}
