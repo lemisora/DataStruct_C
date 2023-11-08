8fd965310417ffc493f0737b2746fe17a3bf26e4
lemisora <leonardo.michel.dom@gmail.com>
2023-11-07 18:44:48 -0600

    Se añade el código fuente de arboles

---
+++ arboles.c
@@ -0,0 +1,22 @@
+/* Programa. Arboles binarios
+ * Alumno: Leonardo Michel Domingo Sánchez
+ * Resumen ++Más info++
+ * - En este caso cada nodo tiene máximo dos hijos
+ * */
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodo Nodo;
+
+struct nodo{
+  Nodo *padre; 
+  Nodo *hijo_izq;
+  Nodo *hijo_der;
+};
+
+int main(int argc, char *argv[]){
+     
+  return EXIT_SUCCESS;
+}
